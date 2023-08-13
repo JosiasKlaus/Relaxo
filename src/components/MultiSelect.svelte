ad78bcc7b23c8ec963bb6b84c940d5476bded55c
Josias Klaus <josias.klaus@web.de>
2023-08-13 23:46:42 +0200

    Added required option to MultiSelect component 💢

--- src/components/MultiSelect.svelte
+++ src/components/MultiSelect.svelte
@@ -6,12 +6,14 @@
   export let label: string;
   export let description: string;
   export let name: string;
+  export let required: boolean;
 
   function onOptionSelect(event: Event) {
     const target = event.target as HTMLSelectElement;
     values.push(target.value);
     options = options.filter((item) => item !== target.value);
     values = values;
+    target.value = "";
   }
 
   function onOptionRemove(option: string) {
@@ -22,7 +24,7 @@
 </script>
 
 <input type="hidden" {name} bind:value={values} />
-<NativeSelect data={options} {label} {description} on:change={onOptionSelect} />
+<NativeSelect data={options} {label} {description} required={required && values.length == 0} placeholder="Bitte auswählen" on:change={onOptionSelect} />
 {#if values.length > 0}
   <Group spacing="md" style="margin-top: 0.5rem;">
     {#each values as item}
@@ -30,7 +32,7 @@
         checked={false}
         label={item}
         variant="filled"
-        on:change={() => {
+        on:click={() => {
           onOptionRemove(item);
         }}
       />
