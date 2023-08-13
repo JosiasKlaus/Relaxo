da47ab40ecf53fdf538f05a1448982cf9747de4d
Josias Klaus <josias.klaus@web.de>
2023-08-13 23:46:21 +0200

    Added model for school information 🏫

---
+++ src/models.ts
@@ -0,0 +1,11 @@
+type School = {
+    type?: string;
+    supervisor?: string;
+    legal?: string;
+    name?: string;
+    address?: string;
+    zipcode?: string;
+    city?: string;
+    email?: string;
+    phone?: string;
+}
