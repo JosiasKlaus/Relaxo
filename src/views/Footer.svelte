5a3280b7e566707eb4de7253b705b339c2385399
Josias Klaus <josias.klaus@web.de>
2023-08-15 22:31:16 +0200

    Seperated header & footer into own components 🔥

---
+++ src/views/Footer.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { Button, Checkbox, Space, Stack, Text } from "@svelteuidev/core";
+    import { staff_cost, material_cost } from "../service";
+
+    const formater = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+    });
+</script>
+
+<Stack>
+    <Text size="lg" weight={"bold"} align="justify" style="line-height: 1.5;">
+        Hiermit beantrage ich für die Umsetzung des Landesprogramms „Löwenstark
+        – der BildungsKICK“ zusätzliche Mittel für in Höhe von
+    </Text>
+    <Text size="lg" style="line-height: 2;">
+        {formater.format($staff_cost)} für Personalausgaben und<br />
+        {formater.format($material_cost)} für Sachausgaben
+    </Text>
+    <Text size="lg" align="justify" style="line-height: 1.5;">
+        Bitte senden Sie das Formular nach dem Ausfüllen ab. Drucken Sie das
+        Formular aus und senden Sie den unterschriebenen Ausdruck an:
+    </Text>
+    <Text size="lg" weight={"bold"} align="justify" style="line-height: 1.5;">
+        Hessisches Kultusministerium<br />
+        Referat I.3<br />
+        z. Hd. Herrn Florian Dierschke<br />
+        Luisenplatz 10<br />
+        65185 Wiesbaden<br />
+    </Text>
+
+    <Text size="lg" align="justify" style="line-height: 1.5;">
+        Die Durchführung aller Angebote steht unter dem Vorbehalt der im
+        Haushalt verfügbaren Mittel des aktuellen Haushaltsjahres. Bei der
+        Antragsprüfung wird das Datum des elektronischen Eingangs im Hessischen
+        Kultusministerium berücksichtigt.
+    </Text>
+    <Space h="md" />
+    <Stack spacing="xl">
+        <Checkbox
+            label="Ich habe die Datenschutzbestimmungen gelesen und erkläre mich damit einverstanden."
+            name="terms_and_conditions"
+        />
+        <Button style="margin-left: auto; max-width: fit-content;" type="submit">Abschicken</Button>
+    </Stack>
+</Stack>
---
+++ src/views/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { Space, Stack, Text, Title } from "@svelteuidev/core";
+</script>
+
+<Stack>
+    <Title order={1}>Antragsformular Löwenstark</Title>
+    <Space h="md" />
+    <Text size="lg" align="justify" style="line-height: 1.5;">
+        Das Hessische Landesprogramm „Löwenstark – der BildungsKICK“ unterstützt
+        Schülerinnen und Schüler dabei, die entstandenen Lernrückstände infolge
+        der Corona-Pandemie zu kompensieren. Zu Beginn des Schuljahres 2023/2024
+        besteht für öffentliche Schulen die Möglichkeit, zusätzliche Mittel (für
+        Personal- oder Sachkosten) zur Kompensation pandemiebedingter
+        Förderbedarfe zu beantragen.
+    </Text>
+    <Text size="lg" weight={"bold"} align="justify" style="line-height: 1.5;">
+        Wichtig! Bitte beachten Sie, dass für die Verausgabung von Maßnahmen
+        entsprechend des Erlasses vom 01.08.2023 „Dezentrale
+        Kompensationsmaßnahmen für pandemiebedingte Förderbedarfe“ grundsätzlich
+        gilt, dass es sich um eine Maßnahme zur Kompensation pandemiebedingter
+        Förderbedarfe handeln muss, damit diese über das zweckgebundene Budget
+        finanziert werden kann. Eine Abstimmung mit dem zuständigen staatlichen
+        Schulamt ist wie je nach Art und Umfang der Maßnahme erforderlich.
+    </Text>
+</Stack>
