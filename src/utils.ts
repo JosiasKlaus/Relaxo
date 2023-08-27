d08530e4ec70f77575df76ddd9a04ceb90a36fb7
Josias Klaus <josias.klaus@web.de>
2023-08-27 20:32:21 +0200

    Add function to create nested object from from 📋

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,28 @@ export const currencyFormater = new Intl.NumberFormat("de-DE", {
     style: "currency",
     currency: "EUR",
 });
+
+export function createNestedObject(input: Record<string, any>): Record<string, any> {
+    const result: Record<string, any> = {};
+
+    for (const key in input) {
+        const keyParts = key.split('_');
+        let currentObj = result;
+
+        for (let i = 0; i < keyParts.length; i++) {
+            const keyPart = keyParts[i];
+
+            if (!currentObj[keyPart]) {
+                if (i === keyParts.length - 1) {
+                    currentObj[keyPart] = input[key];
+                } else {
+                    currentObj[keyPart] = {};
+                }
+            }
+
+            currentObj = currentObj[keyPart];
+        }
+    }
+
+    return result;
+}
