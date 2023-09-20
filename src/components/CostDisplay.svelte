8075991dcca925f6b73b8f8779a2e9df27999ae8
Josias Klaus <josias.klaus@web.de>
2023-09-20 13:21:14 +0200

    Fixed cost entry cost sums 💶

--- src/components/CostDisplay.svelte
+++ src/components/CostDisplay.svelte
@@ -4,28 +4,32 @@
   import { t, c } from "../utils/i18n";
 
   export let year: string;
-  let staff_cost: number, material_cost: number;
+  let staff_cost_year: number, material_cost_year: number;
 
   $: $application, calculateCost();
 
   function calculateCost(): void {
-    staff_cost = 0;
+    staff_cost_year = 0;
     $application.entries?.forEach((entry) => {
+      let staff_cost = 0;
       if (entry.year === year) {
         entry.staff.forEach((staff) => {
           staff_cost += staff.cost || 0;
         });
         entry.summary.staff = staff_cost;
+        staff_cost_year += staff_cost;
       }
     });
 
-    material_cost = 0;
+    material_cost_year = 0;
     $application.entries?.forEach((entry) => {
+      let material_cost = 0;
       if (entry.year === year) {
         entry.material.forEach((material) => {
           material_cost += material.cost || 0;
         });
         entry.summary.material = material_cost;
+        material_cost_year += material_cost;
       }
     });
   }
@@ -33,8 +37,8 @@
 
 <Divider label={$t("cost.title", { year: year })} labelPosition="center" />
 <Group grow noWrap>
-  <TextInput label={$t("cost.staff")} placeholder="0" value={c(staff_cost)} disabled />
-  <TextInput label={$t("cost.surcharge")} placeholder="0" value={c(staff_cost * 1.15)} disabled />
-  <TextInput label={$t("cost.material")} placeholder="0" value={c(material_cost)} disabled />
+  <TextInput label={$t("cost.staff")} placeholder="0" value={c(staff_cost_year)} disabled />
+  <TextInput label={$t("cost.surcharge")} placeholder="0" value={c(staff_cost_year * 1.15)} disabled />
+  <TextInput label={$t("cost.material")} placeholder="0" value={c(material_cost_year)} disabled />
 </Group>
-<TextInput label={$t("cost.total")} placeholder="0" disabled value={c(staff_cost * 1.15 + material_cost)} />
+<TextInput label={$t("cost.total")} placeholder="0" disabled value={c(staff_cost_year * 1.15 + material_cost_year)} />
